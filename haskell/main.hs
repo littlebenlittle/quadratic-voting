5d5ac00e53ece8f7043110c6e916d12b910a90f2
Ben Little <ben.little@fruition.net>
2020-10-18 15:51:55 -0600

    Implement initial state checking

--- haskell/main.hs
+++ haskell/main.hs
@@ -17,6 +17,16 @@ instance Num Nat where
   Suc a + b = Suc (a + b)
   Zero  * a  = a
   Suc a * b  = b + a * b
+  fromInteger x = if x < 0
+                     then error ("cannot parse " ++ show x ++ " as Nat")
+                     else intrec x (\n ->  Suc n) Zero
+
+intrec :: Integer -> (a -> a) -> a -> a
+intrec 0 _ start = start
+intrec i fn start
+  = if i < 0
+      then error "no recursion for integers less than 0"
+      else fn (intrec (i-1) fn start)
 
 instance Ord Nat where
   Zero    <= a = True
@@ -36,6 +46,12 @@ appendMap Map { keys=ks, vals=vs } key val
 
 entries :: Map a b -> [b]
 entries Map { keys=_, vals=vs } = vs
+
+mapLookUp :: (Eq a) => Map a b -> a -> b
+mapLookUp Map { keys=(k:ks), vals=(v:vs) } key
+  = if k == key
+       then v
+       else mapLookUp Map { keys=ks, vals=vs } key
 
 type Id = Nat
 
@@ -64,7 +80,7 @@ data IssueLedgerEntry = IssueLedgerEntry {
 
 data AltLedgerEntry = AltLedgerEntry {
   altdesc      :: AlternativeDescription,
-  votesfor     :: VotesFor,
+  votesFor     :: VotesFor,
   votesAgainst :: VotesAgainst
 } deriving (Show, Read)
 
@@ -75,36 +91,30 @@ data Poll = Poll {
 } deriving (Show, Read)
 
 newPoll :: Poll
-newPoll = Poll {
-  voters = newMap,
-  issues = newMap,
-  alts   = newMap
-}
+newPoll = Poll { voters=newMap, issues=newMap, alts=newMap }
 
 newIssue :: IssueDescription -> IssueLedgerEntry
 newIssue desc = IssueLedgerEntry { issdesc=desc, issalts=[] }
 
 commit :: Backend -> Poll -> IO (Maybe Error)
-commit File { filepath = p, handle = h } state' = do
+commit File { filepath = p } poll' = do
   content <- readFile p
+  putStrLn "AAA"
   case (readMaybe content :: Maybe Poll) of
     Nothing -> return $ Just Error { errmsg = "could not parse state" }
-    Just state ->
-      if isValidTransition state state'
+    Just poll -> do
+      if isValidTransition poll poll'
          then do
-           hPutStr h (show state')
+           putStrLn "CCC"
+           handle <- openFile p WriteMode
+           hPutStr handle (show poll')
+           hClose handle
            return Nothing
          else return $ Just Error { errmsg = "state transition is invalid" }
 commit _ _ = do return $ Just Error { errmsg = "not implemented" }
 
---for each issue, the square of the number of voice credits spent
---is equal to the sums of the squares of all the votes cast
---
---if a voice credit balance changes, the transaction must be
---signed by all voters whose balance changed
-
 data Backend
-  = File     { filepath :: FilePath, handle :: Handle }
+  = File     { filepath :: FilePath }
   | TCP      { url :: String }
   | Database { url :: String }
   | GRPC     { url :: String }
@@ -115,10 +125,11 @@ main = do
   (file, handle) <- openTempFile dir "poll-state"
   putStrLn $ "using temp file " ++ file
   let poll = newPoll
-      backend = File { filepath = file, handle = handle }
+      backend = File { filepath = file }
    in do
       putStrLn "writing initial state to file"
       hPutStr handle $ show poll
+      hClose handle
       putStrLn "applying user transformation"
       applyUserTransformation backend poll
       removeFile file
@@ -201,9 +212,8 @@ inBalance v = (vcAllocated v) <= (vcBalance v)
 
 noAllocationExceedsBalance :: Poll -> Bool
 noAllocationExceedsBalance poll
-  = let ledgerEntries :: [VoterLedgerEntry]
-        ledgerEntries = entries $ voters poll
-     in all inBalance ledgerEntries
+  = let voterEntries = entries $ voters poll
+     in all inBalance voterEntries
 
 allocatedVoiceCreditsAndCostsBalance :: Poll -> Bool
 allocatedVoiceCreditsAndCostsBalance poll
@@ -216,18 +226,31 @@ isValidState state
 
 totalVoiceCreditsAllocated :: Poll -> Nat
 totalVoiceCreditsAllocated state
-  = let ledgerEntry = entries (voters state)
-     in sum (map vcAllocated ledgerEntry)
+  = let voterEntries = entries (voters state)
+     in sum (map vcAllocated voterEntries)
+
+costOfAlternative :: Poll -> AlternativeId -> Nat
+costOfAlternative poll altId
+ = let alt     = mapLookUp (alts poll) altId 
+       for     = votesFor alt
+       against = votesAgainst alt
+    in for*for + against*against
+
+costOfIssue :: Poll -> IssueId -> Nat
+costOfIssue poll issueId
+  = let issue = mapLookUp (issues poll) issueId 
+        alts  = issalts issue
+     in sum $ map (costOfAlternative poll) alts
 
 totalCostOfAllIssues :: Poll -> Nat
-totalCostOfAllIssues state
-  = error "not implemented"
+totalCostOfAllIssues poll
+  = let issueEntries = keys $ issues poll
+     in sum (map (costOfIssue poll) issueEntries)
 
 isValidTransition :: Poll -> Poll -> Bool
 isValidTransition poll poll'
-  = let ledgerEntries :: [VoterLedgerEntry]
-        ledgerEntries = entries $ voters poll
-        sumOfAllBalances = \x -> sum ( map vcBalance ledgerEntries )
+  = let voterEntries = entries $ voters poll
+        sumOfAllBalances = \x -> sum ( map vcBalance voterEntries )
      in isValidState poll'
      && (sumOfAllBalances poll) == (sumOfAllBalances poll')
 
